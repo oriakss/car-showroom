917bbafdc9a45b45f5971d07873c7f3b2ce8883c
oriakss <hellishsaiyan@outlook.com>
2024-12-04 23:59:49 +0300

    added constraint to field "name"

--- src/main/java/ru/clevertec/entity/Category.java
+++ src/main/java/ru/clevertec/entity/Category.java
@@ -31,7 +31,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false, unique = true)
     private String name;
 
     @OneToMany(mappedBy = "category")
